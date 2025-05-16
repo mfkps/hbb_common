5acc1dcac949c0d0429e38d9c2d549d004761de4
mfkps <mfkpsz@qq.com>
2025-05-16 20:39:53 +0800

    修改中继服务器域名和 key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rs.nnbjy.cn"];
+pub const RS_PUB_KEY: &str = "zEfNDuy6hnv6tRALXY0jOL1ys9KsDFttC7+uCwDv4eA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
